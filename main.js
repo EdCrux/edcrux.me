c0132587042c871e232c3320bd464a074c744de7
Eduardo <jecruxiz@gmail.com>
2020-06-24 12:15:16 -0500

    :dizzy: Update animation for the menu
    changing jquery to vanilla javascript

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+
+/*
 $(document).ready(function () {
   $('.menu-toggler').on('click', function () {
+    console.log($(this));
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
+
+  $('.nav-link').on('click', ()=> {
+    $('.menu-toggler').toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  })
 });
+*/
+
+const menu = document.querySelector('.menu-toggler');
+const top_nav = document.querySelector('.top-nav');
+const nav_link = document.querySelector('.nav-link');
+
+menu.addEventListener('click', ()=> {
+  menu.classList.toggle('open');
+  top_nav.classList.toggle('open');
+});
+
+nav_link.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  top_nav.classList.toggle('open');
+})
 
