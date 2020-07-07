6995b22b62f44a7ee40d3bf515bc3bb0c5205620
Eduardo <jecruxiz@gmail.com>
2020-07-07 00:48:13 -0500

    :dizzy: Add animation for the items

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
-  $('.menu-toggler').on('click', function () {
-    $(this).toggleClass('open');
-    $('.top-nav').toggleClass('open');
-  });
-});
+const menu_toggler = document.querySelector('.menu-toggler');
+const top_nav = document.querySelector('.top-nav');
+const nav_link = document.querySelectorAll('.nav-link');
+
+
+menu_toggler.addEventListener('click', () => {
+  menu_toggler.classList.toggle('open');
+  top_nav.classList.toggle('open');
+})
+
+
+nav_link.forEach( link => {
+  link.addEventListener('click', () => {
+    menu_toggler.classList.toggle('open');
+    top_nav.classList.toggle('open');
+  })
+})
 
